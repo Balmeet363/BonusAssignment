c6754b8b0c9859e10325fdc77c1758c6eef58ed9
Balmeet <Balmeet@HP-PC>
2019-10-12 07:40:09 +0530

    Add project files.

---
+++ Bonus-Assignment/question1.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="question1.aspx.cs" Inherits="Bonus_Assignment.question1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <section>
+                <!-- Program heading -->
+                <h2>Program to check Quadrant</h2>
+                <!--Allowing user to enter X-axis and Y-axis value in text boxes and validating user input -->
+                <p>Enter an X axis value:</p>
+                <asp:TextBox runat="server" ID="Xaxis_value"></asp:TextBox>
+                <asp:RequiredFieldValidator 
+                     runat="server" 
+                     EnableClientScript="true" 
+                     ErrorMessage="Please enter X-axis Value" 
+                     ControlToValidate="Xaxis_Value">
+                </asp:RequiredFieldValidator>
+                <p>Enter an Y axis value:</p>
+                <asp:TextBox runat="server" ID="Yaxis_value"></asp:TextBox>
+                <asp:RequiredFieldValidator 
+                     runat="server" 
+                     EnableClientScript="true" 
+                     ErrorMessage="Please enter Y-axis Value" 
+                     ControlToValidate="Yaxis_Value">
+                </asp:RequiredFieldValidator>
+            </section>
+            <!--Creating blank div to generate message which quadrant coordinate would fall after clicking on button-->
+            <div id="coordinate_summary" runat="server"></div>
+            <asp:Button runat="server" text="submit" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Bonus-Assignment/question1.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Bonus_Assignment
+{
+    public partial class question1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack)
+            {
+                coordinate_summary.InnerHtml = "";
+                //collecting X-axis and Y-axis value
+
+                int xaxis_value = Convert.ToInt32(Xaxis_value.Text);
+                int yaxis_value = Convert.ToInt32(Yaxis_value.Text);
+
+                //How do I know if the values are in the Ist Quadrant?
+                if (xaxis_value > 0)
+                {
+                    if (yaxis_value > 0)
+                    {
+                        coordinate_summary.InnerHtml = "Values (" + xaxis_value + "," + yaxis_value + ") are in 1st Quadrant";
+                    }
+                    //How do I know if the values are in the 4th Quadrant ?
+                    else if (yaxis_value < 0)
+                    {
+                        coordinate_summary.InnerHtml = "Values (" + xaxis_value + "," + yaxis_value + ") are in 4th Quadrant";
+                    }
+                    //How do I know if values are on X-axis?
+                    else
+                    {
+                        coordinate_summary.InnerHtml = "Values (" + xaxis_value + "," + yaxis_value + ") are on X-axis";
+                    }
+                }
+
+                //How do I know if the values are in the 2nd Quadrant ?
+                else if (xaxis_value < 0)
+                {
+                    if (yaxis_value > 0)
+                    {
+                        coordinate_summary.InnerHtml = "Values (" + xaxis_value + "," + yaxis_value + ") are in 2nd Quadrant";
+                    }
+                    ////How do I know if the values are in the 3rd Quadrant ?
+                    else if (yaxis_value < 0)
+                    {
+                        coordinate_summary.InnerHtml = "Values (" + xaxis_value + "," + yaxis_value + ") are in 3rd Quadrant";
+                    }
+                    //How do I know if the values are on X-axis ?
+                    else
+                    {
+                        coordinate_summary.InnerHtml = "Values (" + xaxis_value + "," + yaxis_value + ") are on X-axis";
+                    }
+                }
+
+                //How do I know if the values are on Y-axis?
+                else
+                {
+                    if (yaxis_value > 0 || yaxis_value < 0)
+                    {
+                        coordinate_summary.InnerHtml = "Values (" + xaxis_value + "," + yaxis_value + ") are on Y-axis";
+                    }
+                    //how do I know values are at origin?
+                    else
+                    {
+                        coordinate_summary.InnerHtml = "Values (" + xaxis_value + "," + yaxis_value + ") are at origin";
+
+                    }
+                }
+
+            }
+
+        }
+    }
+}
